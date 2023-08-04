a2c694e3a731c4c2e473b7f56b97514a0d5e7406
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-08-04 16:19:23 +0530

    Create Browser Route for the different pages

---
+++ q1_starter/src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { useState } from "react";
+
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Contact } from "./pages/Contact";
+import { createBrowserRouter ,RouterProvider } from "react-router-dom";
+
+export default function App() {
+  // const [page, setPage] = useState("home");
+  const router = createBrowserRouter([
+    {
+      path:'/', element:<Home/>
+    },
+    {
+      path:'/list' ,element:<List/>
+    },
+    {
+      path:'/contact',element:<Contact/>
+    }
+  ])
+  return (
+    <div className="App">
+      {/* <Navbar setPage={setPage} />
+      {page === "home" && <Home />}
+      {page === "list" && <List />}
+      {page === "contact" && <Contact />} */}
+      <Navbar/>
+      <RouterProvider router={router}/>
+      
+    </div>
+  );
+}
