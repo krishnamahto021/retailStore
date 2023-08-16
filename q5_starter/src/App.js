86cbb7f7377990c70f4bdd693f767125bc5379f9
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-08-16 06:51:21 +0530

    Refactor the code to highlight the active page using the NavLink element.

---
+++ q5_starter/src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Contact } from "./pages/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navbar />,
+      children: [
+        {
+          index: true, element: <Home />
+        },
+        {
+          path: '/list', element: <List />
+        },
+        {
+          path: '/contact', element: <Contact />
+        },
+        {
+          
+        }
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
