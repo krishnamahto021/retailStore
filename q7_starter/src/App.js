ed8e284aa24d211aaf87c6c7641a8fd1f09bf0ea
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-08-18 06:13:17 +0530

    Add 404 page to the webApp

---
+++ q7_starter/src/App.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Contact } from "./pages/Contact";
+import { ItemDetails } from "./pages/ItemDetails";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navbar />,
+      errorElement:<NotFound/>,
+      children: [
+        {
+          index: true, element: <Home />
+        },
+        {
+          path: 'list', element: <List />
+        },
+        {
+          path: 'contact', element: <Contact />
+        },
+        {
+          path:'list/:itemId',element:<ItemDetails/>
+        }
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
